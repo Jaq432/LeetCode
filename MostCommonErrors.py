e3ac74ca6caeba96a5a6327380c77282c2af9b2e
Jaq432 <jacobreidth@hotmail.com>
2026-03-07 20:02:21 -0800

    MostCommonError

---
+++ MostCommonErrors.py
@@ -0,0 +1,23 @@
+logs = [
+"INFO request started",
+"ERROR disk full",
+"INFO request finished",
+"ERROR disk full",
+"ERROR timeout",
+"INFO health check",
+"ERROR timeout"
+]
+
+logCounts = {}
+
+for log in logs:
+    if not log in logCounts:
+        logCounts[log] = 1
+        continue
+    logCounts[log] = logCounts[log] + 1
+    
+
+sortedLogCounts = dict(sorted(logCounts.items(), key=lambda x:x[1], reverse=True))
+
+print(logCounts)
+print(sortedLogCounts)
