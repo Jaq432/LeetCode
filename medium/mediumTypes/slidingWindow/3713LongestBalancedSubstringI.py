5f2bbb25df30cd32055dfe1cc5bd3051872b34e8
Jaq432 <jacobreidth@hotmail.com>
2026-03-29 11:47:19 -0700

    Practice

--- medium/mediumTypes/slidingWindow/3713LongestBalancedSubstringI.py
+++ medium/mediumTypes/slidingWindow/3713LongestBalancedSubstringI.py
@@ -8,100 +8,37 @@ Return the length of the longest balanced substring of s.
 from collections import defaultdict
 
 
-def longestBalancedString2(s: str) -> int:
-    slen = len(s)
-    longestLen = 0
-    pointer1 = 0
-    pointer2 = 0
-    for i in range(slen):
-        while 
-
-
-
-
-
-
-
-
-
-
-
-
-
-def longestBalancedSubstring(s: str) -> int:
-    sLen = len(s)
-    longestBalancedLength = 0
-    for i in range(sLen):
-        letterCountDict = defaultdict(int)
-        for j in range(i,sLen):
-            letterCountDict[s[j]] += 1
-        if len(set(letterCountDict.values())) == 1:
-            longestBalancedLength = max(longestBalancedLength, j-i+1)
-    return longestBalancedLength
-
-
-
-
 def longestBalanced(s: str) -> int:
+    slen = len(s)
+    longestBalancedLen = 0
+    for i in range(slen):
+        charDict = defaultdict(int)
+        for j in range(i,slen):
+            charDict[s[j]] += 1
+            if len(set(charDict.values())) == 1:
+                longestBalancedLen = max(longestBalancedLen, j-i+1)
+    return longestBalancedLen
+
+
+def longestBalanced2(s: str) -> int:
     sLen = len(s)
     longestStringNum = 0
+    # Go over the string of chars
     for i in range(sLen):
+        # Create an empty dictionary with built in features
         holdingDict = defaultdict(int)
+        # Go over all of the remaining characters
         for j in range(i,sLen):
+            # Add all of the characters to the dictionary
             holdingDict[s[j]] += 1
-        if len(set(holdingDict.values())) == 1:
-            longestStringNum = max(longestStringNum, j-i+1)
+            # If all of the values are the same
+            if len(set(holdingDict.values())) == 1:
+                # Check if the latest version of the dict is balanced
+                longestStringNum = max(longestStringNum, j-i+1)
     return longestStringNum
 
 
 
 print(longestBalanced("abbac"))     # 4
 print(longestBalanced("zzabccy"))   # 4
-print(longestBalanced("aba"))       # 4
-
-'''
-def longestBalanced(s: str) -> int:
-    sLen = len(s)
-    longestBalancedLength = 0
-
-    for i in range(sLen):
-        print("a")
-        currentLength = 0
-        currentCharMap = {}
-        isBalanced = False
-
-        remainingS = len(s[i:])
-        # Go through all of the possible substrings and add their char and repeat value to dict
-        for j in range(remainingS):
-            print("b")
-            if s[i+j] not in currentCharMap.keys():
-                print("new char")
-                currentCharMap[s[i+j]] = 1
-            else:
-                print(f"existing char {s[i+j]}")
-                currentCharMap[s[i+j]] += 1
-
-        referenceVal = currentCharMap[s[i+j]]
-
-        print(f"currentCharMap: {currentCharMap}")
-
-        print(f"reference val: {referenceVal}")
-
-        # check all l values to make sure they are the same
-        for l in currentCharMap.values():
-            if l != referenceVal:
-                print("broke")
-                break
-        else:
-            print("is balanced")
-            isBalanced = True
-
-        if currentLength > longestBalancedLength and isBalanced:
-            print("updating longestBalancedLength")
-            longestBalancedLength = currentLength
-
-    return longestBalancedLength
-
-print(longestBalanced("abbac"))     # 4
-print(longestBalanced("zzabccy"))   # 4
-print(longestBalanced("aba"))       # 4'''
+print(longestBalanced("aba"))       # 2
